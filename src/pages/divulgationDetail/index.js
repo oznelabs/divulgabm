2192bb1d38323ef33f686f04dd30bea2f2a0f2db
Israel <israelwebti@gmail.com>
2020-03-31 07:41:14 -0300

    add Splash Screen & Logo Icon and Create page structure

---
+++ src/pages/divulgationDetail/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import style from './style';
+
+export default function DivulgationDetail() {
+  return (
+    <View style={style.container}>
+      <View style={style.header}>
+        <Text style={style.logo}>
+          DIVULGA
+        </Text>
+      </View>
+    </View>
+  );
+}
